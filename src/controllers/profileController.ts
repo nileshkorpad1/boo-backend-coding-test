66554cfc62d29a5228f3176755518658eaa1c658
Nilesh Korpad <nileshkorpad@gmail.com>
2024-02-07 02:58:14 +0800

    feat: added getProfileById, updateProfile and deleteProfile

--- src/controllers/profileController.ts
+++ src/controllers/profileController.ts
@@ -19,3 +19,43 @@ export const createProfile = async (req: Request, res: Response) => {
     res.status(500).json({ message: error.message });
   }
 };
+
+export const getProfileById = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+    const profile = await Profile.findById(id);
+    if (!profile) {
+      return res.status(404).json({ message: 'Profile not found' });
+    }
+    res.json(profile);
+  } catch (error:any) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+export const updateProfile = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+    const { imageUrl, name, phone, email, description } = req.body;
+    const updatedProfile = await Profile.findByIdAndUpdate(id, { imageUrl, name, phone, email, description }, { new: true });
+    if (!updatedProfile) {
+      return res.status(404).json({ message: 'Profile not found' });
+    }
+    res.json(updatedProfile);
+  } catch (error:any) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+export const deleteProfile = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+    const deletedProfile = await Profile.findByIdAndDelete(id);
+    if (!deletedProfile) {
+      return res.status(404).json({ message: 'Profile not found' });
+    }
+    res.json({ message: 'Profile deleted successfully' });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
