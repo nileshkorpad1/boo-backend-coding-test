fd0f56ee6f65120791eb5e9caab817f11270ab89
Nilesh Korpad <nileshkorpad@gmail.com>
2024-02-07 03:03:29 +0800

    feat: added comment crud functions to routes

--- src/routes/commentRoutes.ts
+++ src/routes/commentRoutes.ts
@@ -1,9 +1,18 @@
 import express from 'express';
-import { getAllComments, createComment } from '../controllers/commentController';
+import {
+  getAllComments,
+  createComment,
+  getCommentById,
+  updateComment,
+  deleteComment
+} from '../controllers/commentController';
 
 const router = express.Router();
 
 router.get('/', getAllComments);
 router.post('/', createComment);
+router.get('/:id', getCommentById); // New route to get comment by ID
+router.put('/:id', updateComment);
+router.delete('/:id', deleteComment);
 
 export default router;
