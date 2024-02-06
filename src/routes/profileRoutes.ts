a5b686135dee392b095280eea09863ef5b6f0118
Nilesh Korpad <nileshkorpad@gmail.com>
2024-02-07 03:05:42 +0800

    feat: added profile routes

--- src/routes/profileRoutes.ts
+++ src/routes/profileRoutes.ts
@@ -1,9 +1,18 @@
 import express from 'express';
-import { getAllProfiles, createProfile } from '../controllers/profileController';
+import {
+  getAllProfiles,
+  createProfile,
+  getProfileById,
+  updateProfile,
+  deleteProfile
+} from '../controllers/profileController';
 
 const router = express.Router();
 
 router.get('/', getAllProfiles);
 router.post('/', createProfile);
+router.get('/:id', getProfileById); // New route to get profile by ID
+router.put('/:id', updateProfile);
+router.delete('/:id', deleteProfile);
 
 export default router;
