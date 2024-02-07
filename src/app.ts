2a467affca8fe163f479103d83d69d8ca0bee3d4
Nilesh Korpad <nileshkorpad@gmail.com>
2024-02-08 00:29:31 +0800

    chore: changed tsconfig

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,12 @@
 import express from 'express';
+import { json } from 'body-parser';
 import profileRouter from './routes/profileRoutes';
 import commentRouter from './routes/commentRoutes';
 import voteRouter from './routes/voteRoutes';
 
 const app = express();
 
-app.use(express.json());
+app.use(json());
 app.use('/api/profiles', profileRouter);
 app.use('/api/comments', commentRouter);
 app.use('/api/votes', voteRouter);
